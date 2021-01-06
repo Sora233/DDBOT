8e8c8c93295b9625db5a6e683eff238888d485b9
sora233 <sora@sora233.me>
2021-01-06 14:49:11 +0800

    format notify

--- lsp/notify.go
+++ lsp/notify.go
@@ -113,7 +113,7 @@ func (l *Lsp) notifyBilibiliNews(bot *bot.Bot, notify *bilibili.ConcernNewsNotif
 			// TODO
 			log.Debugf("not supported")
 		}
-		result = append(result, message.NewText(dynamicUrl))
+		result = append(result, message.NewText(dynamicUrl+"\n"))
 	}
 	return result
 }
