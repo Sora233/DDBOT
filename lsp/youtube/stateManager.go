d492768a05deceebb950602a4021ea7bcfd0a1be
sora233 <sora@sora233.me>
2021-02-05 00:12:06 +0800

    fix func args

--- lsp/youtube/stateManager.go
+++ lsp/youtube/stateManager.go
@@ -44,7 +44,7 @@ func (s *StateManager) Start() error {
 	if err == nil {
 		db.CreateIndex(s.GroupConcernStateKey(), s.GroupConcernStateKey("*"), buntdb.IndexString)
 		db.CreateIndex(s.FreshKey(), s.FreshKey("*"), buntdb.IndexString)
-		db.CreateIndex(s.UserInfoKey(), s.UserInfoKey("*", buntdb.IndexString))
+		db.CreateIndex(s.UserInfoKey(), s.UserInfoKey("*"), buntdb.IndexString)
 		db.CreateIndex(s.ConcernStateKey(), s.ConcernStateKey("*"), buntdb.IndexBinary)
 	}
 	return s.StateManager.Start()
