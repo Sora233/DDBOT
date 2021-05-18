c4ebecbd4b4e4bcbb982a7f45d22d0a7fb4e09ff
sora233 <sora@sora233.me>
2021-05-18 20:21:18 +0800

    add cli option to set Admin role

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Logiase/MiraiGo-Template/bot"
 	"github.com/Logiase/MiraiGo-Template/config"
 	"github.com/Logiase/MiraiGo-Template/utils"
 	"github.com/Sora233/DDBOT/lsp"
+	localdb "github.com/Sora233/DDBOT/lsp/buntdb"
+	"github.com/Sora233/DDBOT/lsp/permission"
 	"github.com/alecthomas/kong"
 	"net/http"
 	_ "net/http/pprof"
@@ -23,15 +26,31 @@ func init() {
 
 func main() {
 	var cli struct {
-		Play           bool `optional:"" help:"run the play function"`
-		Debug          bool `optional:"" help:"enable debug mode"`
-		GenerateDevice bool `optional:"" help:"generate device.json"`
+		Play           bool  `optional:"" help:"run the play function"`
+		Debug          bool  `optional:"" help:"enable debug mode"`
+		GenerateDevice bool  `optional:"" help:"generate device.json"`
+		SetAdmin       int64 `optional:"" help:"set QQ number to Admin"`
 	}
 	kong.Parse(&cli)
 
 	if cli.GenerateDevice {
 		bot.GenRandomDevice()
 		os.Exit(0)
+	}
+
+	if cli.SetAdmin != 0 {
+		if err := localdb.InitBuntDB(); err != nil {
+			fmt.Println("can not init buntdb")
+			os.Exit(1)
+		}
+		defer localdb.Close()
+		sm := permission.NewStateManager()
+		err := sm.GrantRole(cli.SetAdmin, permission.Admin)
+		if err != nil {
+			fmt.Printf("set role failed %v\n", err)
+			os.Exit(1)
+		}
+		return
 	}
 
 	// 快速初始化
