d8870692d94fa748160f18f33ded7114340604fd
sora233 <sora@sora233.me>
2021-08-14 01:56:32 +0800

    update test

---
+++ lsp/bilibili/bilibili_test.go
@@ -0,0 +1,49 @@
+package bilibili
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDynamicUrl(t *testing.T) {
+	DynamicUrl("abc")
+}
+
+func TestBPath(t *testing.T) {
+	BPath(PathXRelationStat)
+}
+
+func TestBVIDUrl(t *testing.T) {
+	BVIDUrl("bv123")
+}
+
+func TestAddUAOption(t *testing.T) {
+	AddUAOption()
+	AddReferOption("bilibili")
+	AddReferOption()
+}
+
+func TestIsAccountGiven(t *testing.T) {
+	assert.False(t, IsAccountGiven())
+	SetAccount("username", "password")
+	assert.True(t, IsAccountGiven())
+}
+
+func TestParseUid(t *testing.T) {
+	uid, err := ParseUid("123")
+	assert.Nil(t, err)
+	assert.EqualValues(t, 123, uid)
+
+	uid, err = ParseUid("UID:456")
+	assert.Nil(t, err)
+	assert.EqualValues(t, 456, uid)
+
+	uid, err = ParseUid("uid:789")
+	assert.Nil(t, err)
+	assert.EqualValues(t, 789, uid)
+}
+
+func TestCookieInfo(t *testing.T) {
+	SetAccount("a", "b")
+	assert.True(t, IsVerifyGiven())
+}
---
+++ lsp/bilibili/card_extension_test.go
@@ -0,0 +1,36 @@
+package bilibili
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func getCard(t DynamicDescType) *Card {
+	return &Card{
+		Card: "{}",
+		Desc: &Card_Desc{
+			Type: t,
+		},
+	}
+}
+
+func TestCard_GetCard(t *testing.T) {
+	_, err := getCard(DynamicDescType_WithImage).GetCardWithImage()
+	assert.Nil(t, err)
+	_, err = getCard(DynamicDescType_WithOrigin).GetCardWithOrig()
+	assert.Nil(t, err)
+	_, err = getCard(DynamicDescType_WithVideo).GetCardWithVideo()
+	assert.Nil(t, err)
+	_, err = getCard(DynamicDescType_TextOnly).GetCardTextOnly()
+	assert.Nil(t, err)
+	_, err = getCard(DynamicDescType_WithPost).GetCardWithPost()
+	assert.Nil(t, err)
+	_, err = getCard(DynamicDescType_WithMusic).GetCardWithMusic()
+	assert.Nil(t, err)
+	_, err = getCard(DynamicDescType_WithSketch).GetCardWithSketch()
+	assert.Nil(t, err)
+	_, err = getCard(DynamicDescType_WithLive).GetCardWithLive()
+	assert.Nil(t, err)
+	_, err = getCard(DynamicDescType_WithLiveV2).GetCardWithLiveV2()
+	assert.Nil(t, err)
+}
