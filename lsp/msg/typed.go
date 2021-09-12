b19e99edf2b9f4aa0c50375c7b0866cd57b83d23
sora233 <sora@sora233.me>
2021-09-12 14:12:27 +0800

    update msg

--- lsp/msg/typed.go
+++ lsp/msg/typed.go
@@ -6,8 +6,12 @@ import (
 )
 
 // TypedElement 根据TargetType选择不同的element，不解决循环问题，使用不当可能导致堆栈溢出
+// 可以同时设置 OnGroup 和 OnPrivate ，发送时会根据目标自动选择
+// 如果只设置了一个，发送另一个时会返回 nil ，即这里什么也不发送
 type TypedElement struct {
-	E map[TargetType]message.IMessageElement
+	//E map[TargetType]message.IMessageElement
+	privateE message.IMessageElement
+	groupE   message.IMessageElement
 }
 
 func NewTypedElement() *TypedElement {
@@ -27,10 +31,16 @@ func (t *TypedElement) Type() message.ElementType {
 }
 
 func (t *TypedElement) PackToElement(client *client.QQClient, target Target) message.IMessageElement {
-	if t.E == nil {
+	if t.privateE == nil && t.groupE == nil {
 		return nil
 	}
-	e := t.E[target.TargetType()]
+	var e message.IMessageElement
+	switch target.TargetType() {
+	case TargetPrivate:
+		e = t.privateE
+	case TargetGroup:
+		e = t.groupE
+	}
 	if e == nil {
 		return e
 	}
@@ -44,17 +54,14 @@ func (t *TypedElement) OnPrivate(e message.IMessageElement) *TypedElement {
 	if t == e {
 		panic("TypedElement can not type self")
 	}
-	if t.E == nil {
-		t.E = make(map[TargetType]message.IMessageElement)
-	}
-	t.E[TargetPrivate] = e
+	t.privateE = e
 	return t
 }
 
 func (t *TypedElement) OnGroup(e message.IMessageElement) *TypedElement {
-	if t.E == nil {
-		t.E = make(map[TargetType]message.IMessageElement)
+	if t == e {
+		panic("TypedElement can not type self")
 	}
-	t.E[TargetGroup] = e
+	t.groupE = e
 	return t
 }
