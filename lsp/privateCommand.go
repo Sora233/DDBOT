fd7c0b331aff591198ca9fd2797019f090928ec8
sora233 <sora@sora233.me>
2021-10-31 01:42:54 +0800

    update text

--- lsp/privateCommand.go
+++ lsp/privateCommand.go
@@ -1116,7 +1116,7 @@ func (c *LspPrivateCommand) HelpCommand() {
 		"以及开启下播推送：\n" +
 		"/config offline_notify 97505 on\n" +
 		"BOT还支持更多功能，详细命令介绍请查看命令文档：\n" +
-		"https://hub.fastgit.org/Sora233/DDBOT/blob/master/EXAMPLE.md\n" +
+		"https://gitee.com/sora233/DDBOT/blob/master/EXAMPLE.md\n" +
 		"使用时请把作者UID换成你需要的UID\n" +
 		"当您完成所有配置后，可以使用/silence命令，让bot专注于推送，在群内发言更少"
 	help2 := "B站专栏介绍：https://www.bilibili.com/read/cv10602230\n" +
