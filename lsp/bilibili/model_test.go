47f62c556ab13bd39d25e32c442661f196992263
sora233 <sora@sora233.me>
2021-10-30 22:26:25 +0800

    update bilibili model test

--- lsp/bilibili/model_test.go
+++ lsp/bilibili/model_test.go
@@ -6,12 +6,42 @@ import (
 	"testing"
 )
 
-func TestConcernLiveNotify_ToMessage(t *testing.T) {
-	notify := newLiveInfo(test.UID1, true, false, false)
-	m := notify.ToMessage()
+func TestConcernLiveNotify(t *testing.T) {
+	liveNotify := newLiveInfo(test.UID1, true, false, false)
+	liveNotify.GroupCode = test.G1
+	m := liveNotify.ToMessage()
 	assert.NotNil(t, m)
 
-	notify.Status = LiveStatus_NoLiving
-	m = notify.ToMessage()
+	assert.Equal(t, Site, liveNotify.Site())
+	assert.NotNil(t, liveNotify.Logger())
+	assert.NotNil(t, Live, liveNotify.Type())
+	assert.Equal(t, test.G1, liveNotify.GetGroupCode())
+	assert.Equal(t, test.UID1, liveNotify.GetUid())
+
+	liveNotify.Status = LiveStatus_NoLiving
+	m = liveNotify.ToMessage()
 	assert.NotNil(t, m)
+
+	newsNotify := newNewsInfo(test.UID1, DynamicDescType_TextOnly)[0]
+	newsNotify.GroupCode = test.G2
+	assert.NotNil(t, newsNotify)
+	assert.NotNil(t, newsNotify.Logger())
+	assert.Equal(t, Site, newsNotify.Site())
+	assert.Equal(t, News, newsNotify.Type())
+	assert.Equal(t, test.UID1, newsNotify.GetUid())
+	assert.Equal(t, test.G2, newsNotify.GetGroupCode())
+	m = newsNotify.ToMessage()
+	assert.NotNil(t, m)
+
+	notifies := newNewsInfo(test.UID1, DynamicDescType_WithVideo, DynamicDescType_WithImage,
+		DynamicDescType_WithPost, DynamicDescType_WithMusic, DynamicDescType_WithSketch, DynamicDescType_WithLive,
+		DynamicDescType_WithLiveV2, DynamicDescType_WithMiss)
+	for _, notify := range notifies {
+		notify.GroupCode = test.G2
+		m = notify.ToMessage()
+		assert.NotNil(t, m)
+		notify.Card.Card = "{}"
+		m = notify.ToMessage()
+		assert.NotNil(t, m)
+	}
 }
