cee60c76c515408dc84fa7485eb6da55d67b9689
sora233 <sora@sora233.me>
2021-11-03 19:18:36 +0800

    clear play func

--- play.go
+++ play.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func play() {
-	c := make(chan interface{})
-	time.AfterFunc(time.Second*2, func() {
-		close(c)
-	})
-	e := <-c
-	fmt.Println(e)
+
 }
