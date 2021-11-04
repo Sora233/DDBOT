b36e6e526f2deffa8f5a7339c6b09071ee599936
sora233 <sora@sora233.me>
2021-11-04 13:32:52 +0800

    update weibo

--- lsp/weibo/concern.go
+++ lsp/weibo/concern.go
@@ -1,10 +1,12 @@
 package weibo
 
 import (
+	"fmt"
 	"github.com/Logiase/MiraiGo-Template/utils"
 	"github.com/Sora233/DDBOT/lsp/concern"
 	"github.com/Sora233/DDBOT/lsp/concern_type"
 	"github.com/Sora233/DDBOT/lsp/mmsg"
+	localutils "github.com/Sora233/DDBOT/utils"
 	"strconv"
 )
 
@@ -19,9 +21,15 @@ func (c *Concern) Start() error {
 		// TODO
 		return nil, nil
 	}))
-	c.StateManager.UseNotifyGeneratorFunc(func(groupCode int64, event concern.Event) []concern.Notify {
-		// TODO
-		return nil
+	c.StateManager.UseNotifyGeneratorFunc(func(groupCode int64, ievent concern.Event) []concern.Notify {
+		var result []concern.Notify
+		switch news := ievent.(type) {
+		case *NewsInfo:
+			for _, n := range NewConcernNewsNotify(groupCode, news) {
+				result = append(result, n)
+			}
+		}
+		return result
 	})
 	return c.StateManager.Start()
 }
@@ -42,24 +50,94 @@ func (c *Concern) ParseId(s string) (interface{}, error) {
 	return strconv.ParseInt(s, 10, 64)
 }
 
-func (c *Concern) Add(ctx mmsg.IMsgCtx, groupCode int64, id interface{}, ctype concern_type.Type) (concern.IdentityInfo, error) {
-	panic("implement me")
+func (c *Concern) Add(ctx mmsg.IMsgCtx, groupCode int64, _id interface{}, ctype concern_type.Type) (concern.IdentityInfo, error) {
+	id := _id.(int64)
+	log := logger.WithFields(localutils.GroupLogFields(groupCode)).WithField("id", id)
+
+	err := c.StateManager.CheckGroupConcern(groupCode, id, ctype)
+	if err != nil {
+		return nil, err
+	}
+	info, err := c.FindOrLoadUserInfo(id)
+	if err != nil {
+		log.Errorf("FindOrLoadUserInfo error %v", err)
+		return nil, fmt.Errorf("查询用户信息失败 %v - %v", id, err)
+	}
+	_, err = c.StateManager.AddGroupConcern(groupCode, id, ctype)
+	if err != nil {
+		return nil, err
+	}
+	return info, nil
 }
 
-func (c *Concern) Remove(ctx mmsg.IMsgCtx, groupCode int64, id interface{}, ctype concern_type.Type) (concern.IdentityInfo, error) {
-	panic("implement me")
+func (c *Concern) Remove(ctx mmsg.IMsgCtx, groupCode int64, _id interface{}, ctype concern_type.Type) (concern.IdentityInfo, error) {
+	id := _id.(int64)
+	identity, _ := c.Get(id)
+	_, err := c.StateManager.RemoveGroupConcern(groupCode, id, ctype)
+	if identity == nil {
+		identity = concern.NewIdentity(_id, "unknown")
+	}
+	return identity, err
 }
 
 func (c *Concern) List(groupCode int64, ctype concern_type.Type) ([]concern.IdentityInfo, []concern_type.Type, error) {
-	panic("implement me")
+	log := logger.WithFields(localutils.GroupLogFields(groupCode))
+
+	_, ids, ctypes, err := c.StateManager.ListConcernState(
+		func(_groupCode int64, id interface{}, p concern_type.Type) bool {
+			return groupCode == _groupCode && p.ContainAny(ctype)
+		})
+	if err != nil {
+		return nil, nil, err
+	}
+	var result = make([]concern.IdentityInfo, 0, len(ids))
+	var resultTypes = make([]concern_type.Type, 0, len(ids))
+	for index, id := range ids {
+		info, err := c.FindOrLoadUserInfo(id.(int64))
+		if err != nil {
+			log.WithField("id", id.(string)).Errorf("FindOrLoadUserInfo failed %v", err)
+			continue
+		}
+		result = append(result, info)
+		resultTypes = append(resultTypes, ctypes[index])
+	}
+	return result, resultTypes, nil
 }
 
 func (c *Concern) Get(id interface{}) (concern.IdentityInfo, error) {
-	panic("implement me")
+	return c.GetUserInfo(id.(int64))
 }
 
 func (c *Concern) GetStateManager() concern.IStateManager {
 	return c.StateManager
+}
+
+func (c *Concern) FindUserInfo(uid int64, load bool) (*UserInfo, error) {
+	if load {
+		profileResp, err := ApiContainerGetIndexProfile(uid)
+		if err != nil {
+			logger.WithField("uid", uid).Errorf("ApiContainerGetIndexProfile error %v", err)
+			return nil, err
+		}
+		err = c.AddUserInfo(&UserInfo{
+			Uid:             uid,
+			Name:            profileResp.GetData().GetUserInfo().GetScreenName(),
+			ProfileImageUrl: profileResp.GetData().GetUserInfo().GetProfileImageUrl(),
+			ProfileUrl:      profileResp.GetData().GetUserInfo().GetProfileUrl(),
+		})
+		if err != nil {
+			logger.WithField("uid", uid).Errorf("AddUserInfo error %v", err)
+		}
+	}
+	return c.GetUserInfo(uid)
+}
+
+func (c *Concern) FindOrLoadUserInfo(uid int64) (*UserInfo, error) {
+	info, _ := c.FindUserInfo(uid, false)
+	if info == nil {
+		return c.FindUserInfo(uid, true)
+	}
+	return info, nil
 }
 
 func NewConcern(notify chan<- concern.Notify) *Concern {
--- lsp/weibo/stateManager.go
+++ lsp/weibo/stateManager.go
@@ -1,13 +1,33 @@
 package weibo
 
-import "github.com/Sora233/DDBOT/lsp/concern"
+import (
+	"errors"
+	"github.com/Sora233/DDBOT/lsp/concern"
+)
 
 type StateManager struct {
 	*concern.StateManager
+	extraKeySet
 }
 
 func NewStateManager(notify chan<- concern.Notify) *StateManager {
 	return &StateManager{
-		concern.NewStateManagerWithInt64ID(Site, notify),
+		StateManager: concern.NewStateManagerWithInt64ID(Site, notify),
 	}
+}
+
+func (s *StateManager) AddUserInfo(info *UserInfo) error {
+	if info == nil {
+		return errors.New("<nil userInfo>")
+	}
+	return s.JsonSave(s.UserInfoKey(info.Uid), info, true)
+}
+
+func (s *StateManager) GetUserInfo(uid int64) (*UserInfo, error) {
+	var userInfo *UserInfo
+	err := s.JsonGet(s.UserInfoKey(uid), &userInfo)
+	if err != nil {
+		userInfo = nil
+	}
+	return userInfo, nil
 }
