2ccd876243d00172198fd0a4de41fdb3aaf6d429
sora233 <sora@sora233.me>
2021-11-04 20:58:28 +0800

    update acfun test

--- lsp/acfun/apiChannelList_test.go
+++ lsp/acfun/apiChannelList_test.go
@@ -1,12 +1,19 @@
 package acfun
 
 import (
+	localutils "github.com/Sora233/DDBOT/utils"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestApiChannelList(t *testing.T) {
-	resp, err := ApiChannelList(100, "")
+	var resp *ApiChannelListResponse
+	var err error
+	localutils.Retry(5, time.Second, func() bool {
+		resp, err = ApiChannelList(100, "")
+		return err == nil
+	})
 	assert.Nil(t, err)
 	assert.NotNil(t, resp)
 }
