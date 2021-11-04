b01f12784ce33c237c509fc1db0eebcfc2286a40
sora233 <sora@sora233.me>
2021-11-04 11:47:30 +0800

    update doc

--- lsp/concern/config.go
+++ lsp/concern/config.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 )
 
+// IConfig 定义了Config的通用接口
 type IConfig interface {
 	GetGroupConcernAt() *GroupConcernAtConfig
 	GetGroupConcernNotify() *GroupConcernNotifyConfig
@@ -12,8 +13,8 @@ type IConfig interface {
 	Hook
 }
 
-// GroupConcernConfig 默认实现了一些逻辑
-// 如果 Notify 有实现 NotifyLiveExt ，则会使用默认逻辑
+// GroupConcernConfig 实现了 IConfig，并附带一些默认逻辑
+// 如果 Notify 有实现 NotifyLiveExt，则会使用默认逻辑
 type GroupConcernConfig struct {
 	DefaultCallback
 	GroupConcernAt     GroupConcernAtConfig     `json:"group_concern_at"`
@@ -21,10 +22,14 @@ type GroupConcernConfig struct {
 	GroupConcernFilter GroupConcernFilterConfig `json:"group_concern_filter"`
 }
 
+// FilterHook 默认为Pass，可以重写这个函数实现自定义的过滤
+// b站推送使用这个Hook来支持配置动态类型的过滤（过滤转发动态等）
 func (g *GroupConcernConfig) FilterHook(notify Notify) *HookResult {
 	return HookResultPass
 }
 
+// AtBeforeHook 默认为Pass
+// 当 Notify 实现了 NotifyLiveExt，则仅有上播(Living && LiveStatusChanged 均为true)的时候会Pass
 func (g *GroupConcernConfig) AtBeforeHook(notify Notify) *HookResult {
 	var result = new(HookResult)
 	if liveExe, ok := notify.(NotifyLiveExt); ok && liveExe.IsLive() {
@@ -40,6 +45,7 @@ func (g *GroupConcernConfig) AtBeforeHook(notify Notify) *HookResult {
 	return HookResultPass
 }
 
+// ShouldSendHook 默认为Pass
 func (g *GroupConcernConfig) ShouldSendHook(notify Notify) *HookResult {
 	var result = new(HookResult)
 	if liveExt, ok := notify.(NotifyLiveExt); ok && liveExt.IsLive() {
@@ -69,18 +75,22 @@ func (g *GroupConcernConfig) ShouldSendHook(notify Notify) *HookResult {
 	return HookResultPass
 }
 
+// GetGroupConcernAt 返回 GroupConcernAtConfig，总是返回 non-nil
 func (g *GroupConcernConfig) GetGroupConcernAt() *GroupConcernAtConfig {
 	return &g.GroupConcernAt
 }
 
+// GetGroupConcernNotify 返回 GroupConcernNotifyConfig，总是返回 non-nil
 func (g *GroupConcernConfig) GetGroupConcernNotify() *GroupConcernNotifyConfig {
 	return &g.GroupConcernNotify
 }
 
+// GetGroupConcernFilter 返回 GroupConcernFilterConfig，总是返回 non-nil
 func (g *GroupConcernConfig) GetGroupConcernFilter() *GroupConcernFilterConfig {
 	return &g.GroupConcernFilter
 }
 
+// ToString 将 GroupConcernConfig 通过json序列化成string
 func (g *GroupConcernConfig) ToString() string {
 	b, e := json.Marshal(g)
 	if e != nil {
@@ -89,6 +99,7 @@ func (g *GroupConcernConfig) ToString() string {
 	return string(b)
 }
 
+// NewGroupConcernConfigFromString 从json格式反序列化 GroupConcernConfig
 func NewGroupConcernConfigFromString(s string) (*GroupConcernConfig, error) {
 	var concernConfig *GroupConcernConfig
 	decoder := json.NewDecoder(strings.NewReader(s))
