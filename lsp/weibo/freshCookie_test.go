5fe724b8d139b303b90cdc5be36bc8566bd79707
sora233 <sora@sora233.me>
2021-11-05 10:42:22 +0800

    update test

--- lsp/weibo/freshCookie_test.go
+++ lsp/weibo/freshCookie_test.go
@@ -1,12 +1,20 @@
 package weibo
 
 import (
+	localutils "github.com/Sora233/DDBOT/utils"
 	"github.com/stretchr/testify/assert"
+	"net/http"
 	"testing"
+	"time"
 )
 
 func TestFreshCookie(t *testing.T) {
-	cookies, err := FreshCookie()
+	var cookies []*http.Cookie
+	var err error
+	localutils.Retry(5, time.Second, func() bool {
+		cookies, err = FreshCookie()
+		return err == nil
+	})
 	assert.Nil(t, err)
 	assert.NotEmpty(t, cookies)
 }
