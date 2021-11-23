5f05d7a7de2dc622cb8da69b9b91355c191c9bc2
sora233 <sora@sora233.me>
2021-11-23 18:28:17 +0800

    fix youtube video notify

--- lsp/youtube/config.go
+++ lsp/youtube/config.go
@@ -8,6 +8,16 @@ type GroupConcernConfig struct {
 	concern.IConfig
 }
 
+func (g *GroupConcernConfig) ShouldSendHook(notify concern.Notify) *concern.HookResult {
+	if c, ok := notify.(*ConcernNotify); ok {
+		// 直播预告也应该推送
+		if c.IsWaiting() {
+			return concern.HookResultPass
+		}
+	}
+	return g.IConfig.ShouldSendHook(notify)
+}
+
 func NewGroupConcernConfig(g concern.IConfig) *GroupConcernConfig {
 	return &GroupConcernConfig{g}
 }
