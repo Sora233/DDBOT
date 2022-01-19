d983eeabe92ec527e39670aceffbe016c7bb4d4c
sora233 <sora@sora233.me>
2022-01-19 12:46:50 +0800

    remove deprecated test

--- lsp/acfun/v2UserContentProfile_test.go
+++ lsp/acfun/v2UserContentProfile_test.go
@@ -1,21 +1,14 @@
 package acfun
 
-import (
-	localutils "github.com/Sora233/DDBOT/utils"
-	"github.com/stretchr/testify/assert"
-	"testing"
-	"time"
-)
-
-func TestV2UserContentProfile(t *testing.T) {
-	var resp *V2UserContentProfileResponse
-	var err error
-	localutils.Retry(5, time.Second, func() bool {
-		resp, err = V2UserContentProfile(1)
-		return err == nil
-	})
-	assert.Nil(t, err)
-	assert.Zero(t, resp.GetErrorid())
-	assert.EqualValues(t, 1, resp.GetVdata().GetUserId())
-	assert.EqualValues(t, "admin", resp.GetVdata().GetUsername())
-}
+//func TestV2UserContentProfile(t *testing.T) {
+//	var resp *V2UserContentProfileResponse
+//	var err error
+//	localutils.Retry(5, time.Second, func() bool {
+//		resp, err = V2UserContentProfile(1)
+//		return err == nil
+//	})
+//	assert.Nil(t, err)
+//	assert.Zero(t, resp.GetErrorid())
+//	assert.EqualValues(t, 1, resp.GetVdata().GetUserId())
+//	assert.EqualValues(t, "admin", resp.GetVdata().GetUsername())
+//}
